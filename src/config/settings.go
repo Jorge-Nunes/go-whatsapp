54c36875b865b4756f17d90ec83f2a2bc9dbfe7a
Jorge-Nunes <jcvn@jcvn.com.br>
2024-10-13 18:35:02 +0000

    first commit

---
+++ src/config/settings.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"go.mau.fi/whatsmeow/proto/waCompanionReg"
+)
+
+var (
+	AppVersion             = "v4.20.0"
+	AppPort                = "3000"
+	AppDebug               = false
+	AppOs                  = "AldinoKemal"
+	AppPlatform            = waCompanionReg.DeviceProps_PlatformType(1)
+	AppBasicAuthCredential string
+
+	PathQrCode    = "statics/qrcode"
+	PathSendItems = "statics/senditems"
+	PathMedia     = "statics/media"
+	PathStorages  = "storages"
+
+	DBName = "whatsapp.db"
+
+	WhatsappAutoReplyMessage    string
+	WhatsappWebhook             string
+	WhatsappWebhookSecret             = "secret"
+	WhatsappLogLevel                  = "ERROR"
+	WhatsappSettingMaxFileSize  int64 = 50000000  // 50MB
+	WhatsappSettingMaxVideoSize int64 = 100000000 // 100MB
+	WhatsappTypeUser                  = "@s.whatsapp.net"
+	WhatsappTypeGroup                 = "@g.us"
+	WhatsappAccountValidation         = true
+)
---
+++ src/domains/user/user.go
@@ -0,0 +1,13 @@
+package user
+
+import (
+	"context"
+)
+
+type IUserService interface {
+	Info(ctx context.Context, request InfoRequest) (response InfoResponse, err error)
+	Avatar(ctx context.Context, request AvatarRequest) (response AvatarResponse, err error)
+	MyListGroups(ctx context.Context) (response MyListGroupsResponse, err error)
+	MyListNewsletter(ctx context.Context) (response MyListNewsletterResponse, err error)
+	MyPrivacySetting(ctx context.Context) (response MyPrivacySettingResponse, err error)
+}
